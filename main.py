bfab841f8ab2dfe3d740448a85e3f95d626086ae
olapok <124047719+olapok@users.noreply.github.com>
2023-09-12 09:55:26 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+number = 0
+
+
+for number in range(0,7):
+    print(number)
+    if number == 6:
+        print("Banana")
+for number in range(8,101):
+    print(number)
+
+print('')
+
+for number in range(100,7,-1):
+    print(number)
+    if number == 8:
+        print("Banana")
+for number in range(6,-1,-1):
+    print(number)
+
+
