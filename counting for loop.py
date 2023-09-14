401747145dae6f8a81b1d1ee02ddff9f2b1c6014
olapok <124047719+olapok@users.noreply.github.com>
2023-09-14 19:48:25 +0100

    Add files via upload

---
+++ counting for loop.py
@@ -0,0 +1,19 @@
+number = 0
+number = str(number)
+s = str(7)
+
+
+for number in range(0,101):
+    if str(7) in str(number):
+        print("Banana")
+    else:
+        print (number)
+
+print('')
+
+for number in range(100,-1,-1):
+    if str(7) in str(number):
+        print("Banana")
+    else:
+        print(number)
+
